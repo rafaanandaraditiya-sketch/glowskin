4e84ba42c99c98fb5f7f18546c5460c937b8c843
GLOWSKIN <rafaanandaraditiya@gmail.com>
2026-07-18 11:07:37 +0700

    update website

--- script.js
+++ script.js
@@ -1,7 +1,54 @@
-const buttons = document.querySelectorAll("button");
+// ===============================
+// Smooth Scroll
+// ===============================
 
-buttons.forEach(button => {
-    button.addEventListener("click", () => {
-        alert("Terima kasih telah mengunjungi GlowSkin 🌿");
+document.querySelectorAll('nav a').forEach(anchor => {
+
+    anchor.addEventListener('click', function(e){
+
+        e.preventDefault();
+
+        const tujuan = document.querySelector(this.getAttribute('href'));
+
+        if(tujuan){
+
+            tujuan.scrollIntoView({
+                behavior:'smooth'
+            });
+
+        }
+
     });
+
+});
+
+// ===============================
+// Tombol Beli
+// ===============================
+
+const tombol = document.querySelectorAll(".card button");
+
+tombol.forEach(btn=>{
+
+    btn.addEventListener("click",()=>{
+
+        window.open(
+            "https://wa.me/6281234567890?text=Halo,%20saya%20ingin%20membeli%20produk%20GlowSkin",
+            "_blank"
+        );
+
+    });
+
+});
+// ============================
+// Menu Hamburger
+// ============================
+
+const menu = document.getElementById("menu-toggle");
+const navMenu = document.querySelector("nav ul");
+
+menu.addEventListener("click",()=>{
+
+    navMenu.classList.toggle("active");
+
 });
